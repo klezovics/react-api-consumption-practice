1a44567bebd9c0781eac0f77a7ae938e9259f70c
artursklezovics <klezovich@gmail.com>
2025-01-31 13:29:40 +0100

    Add fetch and axios implementation

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import ToDoListFetch from "./components/ToDoListFetch.tsx";
+import ToDoListFetchOrAxios from "./components/ToDoListFetchOrAxios.tsx";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <ToDoListFetch/>
+      <ToDoListFetchOrAxios/>
     </>
   )
 }
--- src/components/ToDoListFetchOrAxios.tsx
+++ src/components/ToDoListFetchOrAxios.tsx
@@ -1,15 +1,25 @@
 import React, {useEffect, useState} from 'react';
 import {Todo} from "../model/todos.ts";
 import ToDoList from "./ToDoList.tsx";
+import axios from 'axios';
+import axiosClient from "../api/axiosClient.ts";
 
 
-async function getTodos() {
+// Case 1: Using fetch API to fetch data
+async function getTodosFetch() {
     const response = await fetch('https://jsonplaceholder.typicode.com/todos');
     if (!response.ok) throw new Error(`HTTP Error: ${response.status}`);
     return response.json();
 }
 
-const ToDoListFetch = () => {
+
+// Case 2: Using Axios to fetch data
+async function getTodosAxios() {
+    const response = await axiosClient.get<Todo[]>('https://jsonplaceholder.typicode.com/todos');
+    return response.data;
+}
+
+const ToDoListFetchOrAxios = () => {
 
     const [todos, setTodos] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
@@ -18,7 +28,11 @@ const ToDoListFetch = () => {
         const fetchTodos = async () => {
             setIsLoading(true);
             try {
-                const todos = (await getTodos()).slice(0,10);
+
+                // There is very little difference for component structure if we
+                // use fetch or axios here
+                const todos = (await getTodosFetch()).slice(0,10);
+                // const todos = (await getTodosAxios()).slice(0,10);
                 setTodos(todos);
             } catch (e) {
                 console.error('Error fetching todos:', e);
@@ -37,4 +51,4 @@ const ToDoListFetch = () => {
     );
 };
 
-export default ToDoListFetch;
+export default ToDoListFetchOrAxios;
